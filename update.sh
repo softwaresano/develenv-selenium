797bd26580b74c7ae56fedbfa69212002c81aaf5
carlosg <carlosenrique.gomezgomez@telefonica.com>
2021-01-20 18:45:41 +0100

    LATEST_RELEASE_88

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 minor_version=$(grep "%define selenium_version" src/rpm/SPECS/selenium.spec |awk '{print $3}')
-chromedriver_version=$(curl -f https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+chromedriver_version=$(curl -f https://chromedriver.storage.googleapis.com/LATEST_RELEASE_88)
 geckodriver_version=$(curl -f https://github.com/mozilla/geckodriver/releases/latest|grep -Po  '(?<=tag/).*(?=")')
 [[ "${geckodriver_version}" == '' ]] && exit 1
 major_version=$(echo $minor_version|sed s:"\.[0-9]*$":"":g)
