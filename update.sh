c67a4788a9523343cb8338f8c196d1466605356b
caenco <carlos.encabocorrales@telefonica.com>
2025-10-29 17:25:28 +0100

    Remove deprecated variables

--- update.sh
+++ update.sh
@@ -4,9 +4,7 @@ chromedriver_version=$(grep --color=no -Po '(?<=chromedriver-py="=).*(?=\")' Pip
 geckodriver_version=$(curl -I -f -s https://github.com/mozilla/geckodriver/releases/latest | grep --color=no -Po '(?<=releases/tag/).*(?=\r)')
 [[ ${geckodriver_version} == '' ]] && exit 1
 major_version=$(echo $minor_version | sed s:"\.[0-9]*$":"":g)
-selenium_grid_extras_version=2.0.4
 selenium_url="https://github.com/SeleniumHQ/selenium/releases/download/selenium-${minor_version}/selenium-server-${minor_version}.jar"
-selenium_grid_extras_url=https://github.com/groupon/Selenium-Grid-Extras/releases/download/v${selenium_grid_extras_version}/SeleniumGridExtras-${selenium_grid_extras_version}-SNAPSHOT-jar-with-dependencies.jar
 chrome_driver_url="https://storage.googleapis.com/chrome-for-testing-public/${chromedriver_version:?}/linux64/chromedriver-linux64.zip"
 gecko_driver_url=https://github.com/mozilla/geckodriver/releases/download/${geckodriver_version}/geckodriver-${geckodriver_version}-linux64.tar.gz
 rm -Rf target/artifacts
